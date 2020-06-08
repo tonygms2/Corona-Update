87e445bd54ba8f05e401d6997022f5d4b5779348
Nicholas Tony Gomes <tonygms2@gmail.com>
2020-06-08 17:07:36 +0600

    removed Search and Button, added dropdown menu with search feature

--- CoronaInfoUpdate/app/src/main/java/ulab/edu/coronainfoupdate/MainActivity.java
+++ CoronaInfoUpdate/app/src/main/java/ulab/edu/coronainfoupdate/MainActivity.java
@@ -15,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 import android.net.Uri;
 
+import com.hbb20.CountryCodePicker;
+
 
 public class MainActivity extends AppCompatActivity {
     public static TextView death;
@@ -26,7 +28,7 @@ public class MainActivity extends AppCompatActivity {
     public static TextView TotalCase;
     private static final int REQUEST_CALL=1;
     public static TextView TotalTest;
-
+    public static CountryCodePicker ccp;
     EditText getUrl;
     String url;
     @Override
@@ -42,30 +44,32 @@ public class MainActivity extends AppCompatActivity {
         TotalActiveCase = (TextView)findViewById(R.id.TotalActiveCase);
         TotalCase =(TextView)findViewById(R.id.TotalCase);
         TotalTest = (TextView)findViewById(R.id.TotalTest);
+        ccp = (CountryCodePicker)findViewById(R.id.ccp);
+        url = ccp.getSelectedCountryName().toString();
+        fetchData data = new fetchData(url);
+        data.execute();
+        ccp.setOnCountryChangeListener(new CountryCodePicker.OnCountryChangeListener() {
+            @Override
+            public void onCountrySelected() {
+                url = ccp.getSelectedCountryName().toString();
+                if(url.contains("UAE")){
+                    url = "UAE";
+                    Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
+                }
+                if(url.equals("United States")){
+                    url = "USA";
+                    Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
+                } if(url.equals("United Kingdom")){
+                    url = "UK";
+                    Toast.makeText(MainActivity.this, url, Toast.LENGTH_SHORT).show();
+                }
+                fetchData data = new fetchData(url);
+                data.execute();
+
+
+            }
+        });
     }
-    public void onClick(View view) {
-
-
-        url = getUrl.getText().toString();
-        if(url.length()==0){
-            death.setText("0");
-            infected.setText("0");
-            recovered.setText("0");
-            TotalDeath.setText("0");
-            TotalCase.setText("0");
-            TotalActiveCase.setText("0");
-            TotalTest.setText("0");
-            Toast.makeText(this, "Please Enter A Country Name", Toast.LENGTH_SHORT).show();
-        }else {
-
-            fetchData data = new fetchData(url);
-            data.execute();
-        }
-
-
-
-    }
-
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
